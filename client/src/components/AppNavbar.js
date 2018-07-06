de1cef16ef0ab20683d2f925d74c526386ef5bc9
Tyler Gotz <tyler.gotz@teschglobal.com>
2018-07-06 14:21:41 -0500

    Version 1.0.0

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+class AppNavBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">
+              ShoppingList
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png">
+                    Google
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    )
+  }
+
+}
+
+export default AppNavBar;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://tyler:gotz123@ds227119.mlab.com:27119/mern_shoppinglist'
+}
